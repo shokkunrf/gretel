84f4fa8a0c4033cd263e47cedfc7977397a399af
shokkunrf <19404989+shokkunrf@users.noreply.github.com>
2020-12-20 02:02:57 +0900

    起動するように修正

--- manage.sh
+++ manage.sh
@@ -5,14 +5,14 @@ SUB_COMMAND=$1
 SERVER_PATH="/home/$(whoami)/opt/spigot"
 
 start() {
-  screen -Dm -S spigot -X /usr/bin/java -Xms2048M -Xmx2048M -jar $SERVER_PATH/spigot-server.jar
+  screen -ADmS spigot java -Xms2048M -Xmx2048M -jar $SERVER_PATH/spigot-server.jar nogui
 }
 
 stop() {
-  screen -S spigot -X stuff "This server will be shutdown in 10s\015"
+  screen -S spigot -X stuff "say This server will be shutdown in 10s\n"
   sleep 10
-  screen -S spigot -X stuff "save-all\015"
-  screen -S spigot -X stuff "stop\015"
+  screen -S spigot -X stuff "save-all\n"
+  screen -S spigot -X stuff "stop\n"
 }
 
 case $SUB_COMMAND in
